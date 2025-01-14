5f8511950bb5e107e19ea732008b762da7959709
liyakat ali <aliliyakat902@gmail.com>
2025-01-14 08:23:43 +0800

    All about Dot Product.

--- Linear-Algebra/10-dot-product.py
+++ Linear-Algebra/10-dot-product.py
@@ -8,7 +8,7 @@ x = np.array([25, 2, 5])
 
 y = np.array([0, 1 ,2])
 
-print(25*0 + 2*1 + 5*2)     # Manual
+print(25*0 + 2*1 + 5*2)     # Manual Calculation
 
 print(np.dot(x, y))         # using numpy
 

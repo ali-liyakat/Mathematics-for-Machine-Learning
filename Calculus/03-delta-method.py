7a80076979c9cd54dc87c3c696f73c7b3027787b
liyakat ali <aliliyakat902@gmail.com>
2025-02-03 21:31:31 +0800

    All about Delta method.

---
+++ Calculus/03-delta-method.py
@@ -0,0 +1,100 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+x = np.linspace(-10, 10, 10000) # start, finish, n points
+
+y = x**2 + 2*x + 2
+
+def f(my_x):
+    my_y = my_x**2 + 2*my_x + 2
+    return my_y
+
+y= f(x)
+
+fig, ax = plt.subplots()
+plt.axvline(x=0, color='lightgray')
+plt.axhline(y=0, color='lightgray')
+_ = ax.plot(x,y)
+plt.show()
+
+print(f(2)) # identify slope where, x=2  output, y = 10
+
+fig, ax = plt.subplots()
+plt.axvline(x=0, color='lightgray')
+plt.axhline(y=0, color='lightgray')
+plt.scatter(2, 10) # new
+_ = ax.plot(x,y)
+plt.show()
+
+# The delta method uses the difference between two points to calculate slope. To illustrate this, let's define another point, where x= 5
+
+print(f(5)) #output, y =37
+
+fig, ax = plt.subplots()
+plt.axvline(x=0, color='lightgray')
+plt.axhline(y=0, color='lightgray')
+plt.scatter(2, 10)
+plt.scatter(5, 37, c = 'orange', zorder=3) # new
+_ = ax.plot(x,y)
+plt.show()
+
+m = (37-10)/(5-2)
+print(m)
+
+b = 37-m*5
+print(b)
+
+line_y = m*x + b
+
+fig, ax = plt.subplots()
+plt.axvline(x=0, color='lightgray')
+plt.axhline(y=0, color='lightgray')
+plt.scatter(2, 10)
+plt.scatter(5, 37, c='orange', zorder=3)
+plt.ylim(-5, 150) # new
+plt.plot(x, line_y, c='orange') # new
+_ = ax.plot(x,y)
+plt.show()
+
+print(f(2.1))
+
+fig, ax = plt.subplots()
+plt.axvline(x=0, color='lightgray')
+plt.axhline(y=0, color='lightgray')
+plt.scatter(2, 10)
+plt.scatter(2.1, 10.61, c = 'orange', zorder=3)
+_ = ax.plot(x,y)
+plt.show()
+
+m = (10.61-10)/(2.1-2)
+print(m)
+
+b = 10.61-m*2.1
+print(b)
+
+line_y = m*x + b
+
+fig, ax = plt.subplots()
+plt.axvline(x=0, color='lightgray')
+plt.axhline(y=0, color='lightgray')
+plt.scatter(2, 10)
+plt.scatter(2.1, 10.61, c='orange', zorder=3)
+plt.ylim(-5, 150)
+plt.plot(x, line_y, c='orange', zorder=3)
+_ = ax.plot(x,y)
+plt.show()
+
+delta_x = 0.000001
+print(delta_x)
+
+x1 = 2
+y1 = 10
+
+x2 = x1 + delta_x
+print(x2)
+
+y2 = f(x2)
+print(y2)
+
+m = (y2 - y1)/(x2 - x1)
+print(m)

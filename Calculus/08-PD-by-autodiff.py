1d2bf6e794b7a2c55738f3ab4b33556a086c53a8
liyakat ali <aliliyakat902@gmail.com>
2025-02-07 16:01:56 +0800

    Partial Derivatives by Autodiff.

---
+++ Calculus/08-PD-by-autodiff.py
@@ -0,0 +1,20 @@
+import torch
+
+x = torch.tensor(0.).requires_grad_() 
+print(x)
+
+y = torch.tensor(0.).requires_grad_() 
+print(y)
+
+
+def f(my_x, my_y):
+    return my_x**2 - my_y**2
+
+
+z = f(x, y) # Forward pass
+print(z)
+
+z.backward() # Autodiff
+
+print(x.grad)
+print(y.grad)
